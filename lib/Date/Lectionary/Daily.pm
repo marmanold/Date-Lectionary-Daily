e257330c5e44c7aac7639ed8480cb97e247d94de
Michael Arnold <michael.arnold@lifeway.com>
2017-01-13 15:57:09 -0600

    Start of Date::Lectionary::Daily

---
+++ lib/Date/Lectionary/Daily.pm
@@ -0,0 +1,79 @@
+package Date::Lectionary::Daily;
+
+use v5.22;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Date::Lectionary::Daily - Daily Readings for the Christian Lectionary
+
+=head1 VERSION
+
+Version 1.20170113
+
+=cut
+
+our $VERSION = '1.20170113';
+
+
+=head1 AUTHOR
+
+Michael Wayne Arnold, C<< <marmanold at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-date-lectionary-daily at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Date-Lectionary-Daily>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Date::Lectionary::Daily
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Date-Lectionary-Daily>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Date-Lectionary-Daily>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Date-Lectionary-Daily>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Date-Lectionary-Daily/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Many thanks to my beautiful wife, Jennifer, and my amazing daughter, Rosemary.  But, above all, SOLI DEO GLORIA!
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017 Michael Wayne Arnold.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;    # End of Date::Lectionary::Daily
